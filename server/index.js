b16d847dbbb51ba22cde297573d7b51e1f04bd6a
Shashi <shashiwadananirmani14@gmail.com>
2021-06-13 01:58:30 +0530

    admin generated messages

--- server/index.js
+++ server/index.js
@@ -9,17 +9,22 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connect', (socket) => {
-    console.log('We have a new connection !!!');
+const {addUser, removeUser, getUser, getUsersInRoom} =require( './users.js');
 
+io.on('connection', (socket) => {
+    
     socket.on('join',({name,room},callback) => {
-        console.log(name , room);
-        const error = true;
-        if(error){
-            callback({error:'error'});
-        }
+       const {error, user} = addUser({id : socket.id,name,room});
        
+       if(error) return callback(error);
+
+       socket.emit('message',{user:'admin', text:`$(user.name), welcome to the room $(user.room)`});
+       socket.broadcast.to(user.room).emit('message',{user: 'admin', text:'$(user.name), has joined!'})
+       socket.join(user.room);
+       callback();
     });
+
+    
 
     socket.on('disconnect',() => {
         console.log('User had left!!!')
